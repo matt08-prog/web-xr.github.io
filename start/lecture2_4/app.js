6536425bfd8452f481989af6e71af55f1a81c956
matt08-prog <58103083+matt08-prog@users.noreply.github.com>
2021-02-21 21:38:29 -0700

    push

--- start/lecture2_4/app.js
+++ start/lecture2_4/app.js
@@ -37,7 +37,7 @@ class App{
 
         for (let theta = inc; theta < PI2; theta+= inc){
             const radius = (inner) ? innerRadius : outerRadius
-            shape.lineTo( Math.sin(theta)*radius, Math.cos(theta)* radius)
+            shape.lineTo( Math.cos(theta)*radius, Math.sin(theta)* radius)
             inner = !inner
         }
 
