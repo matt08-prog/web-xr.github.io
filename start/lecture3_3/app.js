fe5941b11642da0b2baf1bac5f20729eb0123b9c
matt08-prog <58103083+matt08-prog@users.noreply.github.com>
2021-02-22 21:36:11 -0700

    push

--- start/lecture3_3/app.js
+++ start/lecture3_3/app.js
@@ -90,7 +90,7 @@ class App{
     
     buildControllers(){
         const controllerModelFactory = new XRControllerModelFactory()
-//testing
+//testing again
         const geometry = new THREE.BufferGeometry().setFromPoints( [
             new THREE.Vector3(0,0,0),
             new THREE.Vector3(0,0,-1)
